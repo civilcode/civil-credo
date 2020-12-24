88ab4b6715ba12fc20e3116ba7db78a8bebbb001
Nicholas Henry <nicholas.henry@gmail.com>
2020-12-24 10:49:40 -0500

    Upgrade credo 1.2 => 1.5

--- test/support/test_application.exs
+++ test/support/test_application.exs
@@ -25,10 +25,8 @@ defmodule Credo.Test.Application do
   use Application
 
   def start(_type, _args) do
-    import Supervisor.Spec, warn: false
-
     children = [
-      worker(Credo.Test.FilenameGenerator, [])
+      Credo.Test.FilenameGenerator
     ]
 
     opts = [strategy: :one_for_one, name: Credo.Test.Application.Supervisor]
